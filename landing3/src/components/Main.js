edebf23eb1b5d3b95cc2638ef5438de54b235adb
hyj5076 <hyj5076@hanmail.net>
2023-09-18 02:32:37 +0900

    New: Add the new function
    window height resize

--- landing3/src/components/Main.js
+++ landing3/src/components/Main.js
@@ -1,18 +1,55 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function Main() {
+    const [sectionHeight, setSectionHeight] = useState(null);
+
+    useEffect(() => {
+        // 처음 로드될 때 섹션 높이 설정
+        const initialSetSectionHeight = () => {
+            let windowWidth = window.innerWidth;
+            let windowHeight = window.innerHeight;
+
+            if (windowWidth >= 500) {
+                setSectionHeight('100vh');
+            } else {
+                setSectionHeight(`${windowHeight}px`);
+            }
+        };
+
+        // 처음 로드 시 기본 창 높이 유지
+        initialSetSectionHeight();
+
+        // 창 높이가 변경될 때마다 실행되는 함수
+        const handleResize = () => {
+            let windowWidth = window.innerWidth;
+
+            //width 500 이상인 환경에서만 resize 됨
+            if (windowWidth >= 500) {
+                setSectionHeight('100vh');
+            }
+        };
+
+        // 리사이즈 이벤트에 함수를 등록
+        window.addEventListener('resize', handleResize);
+
+        // 컴포넌트 언마운트 시 이벤트 리스너 제거
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <div id="main">
             <section id="section01">
-                <div className="style active">
-                    <div class="title">
-                        <div class="inner">
+                <div className="style active" style={{ height: sectionHeight }}>
+                    <div className="title">
+                        <div className="inner">
                             <h1>힐스테이트 푸르지오 수원</h1>
-                            <p class="exhibition">입주전시회</p>
-                            <p class="date">2023. 10. 21 토 - 10. 22 일</p>
-                            <p class="time">10 AM - 6 PM</p>
-                            <div class="btn"><a href="sub/booking.html">참가신청</a></div>
+                            <p className="exhibition">입주전시회</p>
+                            <p className="date">2023. 10. 21 토 - 10. 22 일</p>
+                            <p className="time">10 AM - 6 PM</p>
+                            <div className="btn"><a href="sub/booking.html">참가신청</a></div>
                         </div>
                     </div>
                 </div>
--- landing3/src/components/Timer.js
+++ landing3/src/components/Timer.js
@@ -1,34 +1,34 @@
 import './App.css';
 
 function Timer() {
-  return (
-    <div id="main">
-        <section id="section09">
-            <div class="inner padding">
-                <div class="title">
-                    <div>
-                        <h3>사전예약</h3>
-                        <p id="changeTxt">입주전시회까지 남은 시간</p>
+    return (
+        <div id="main">
+            <section id="section09">
+                <div class="inner padding">
+                    <div class="title">
+                        <div>
+                            <h3>사전예약</h3>
+                            <p id="changeTxt">입주전시회까지 남은 시간</p>
+                        </div>
                     </div>
-                </div>
 
-                <div id="removeTime" class="content">
-                    <div class="timer">
-                        <div class="dday">
-                            <p id="days"></p>
-                            <p><span id="countdown"></span></p>
-                        </div>
-                        <div class="circle">
-                            <div class="subcircle"></div>
-                        </div>
-                        <div class="submitbtn">
-                            <a href="sub/booking.html">참가신청</a>
+                    <div id="removeTime" class="content">
+                        <div class="timer">
+                            <div class="dday">
+                                <p id="days"></p>
+                                <p><span id="countdown"></span></p>
+                            </div>
+                            <div class="circle">
+                                <div class="subcircle"></div>
+                            </div>
+                            <div class="submitbtn">
+                                <a href="sub/booking.html">참가신청</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </div>
+            </section>
+        </div>
   );
 }
 
