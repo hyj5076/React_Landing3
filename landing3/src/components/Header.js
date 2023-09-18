0d2a8f4127a7eb46262bfb83a05f816cad4ae26e
hyj5076 <hyj5076@hanmail.net>
2023-09-19 03:37:56 +0900

    New: Add the new function
    header nav
    popup
    scroll toggleclass

--- landing3/src/components/Header.js
+++ landing3/src/components/Header.js
@@ -1,29 +1,74 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function Header() {
+  const [navVisible, setNavVisible] = useState(false);
+
+  useEffect(() => {
+    const navBtn = document.querySelector('.nav_btn');
+    const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
+    const icon = document.querySelector('.nav_btn i');
+
+    const toggleNav = (e) => {
+      e.preventDefault();
+      if (!navVisible) {
+        nav.style.display = 'block';
+        nav.style.height = `${nav.scrollHeight}px`;
+        icon.classList.remove('bi-list');
+        icon.classList.add('bi-x');
+        header.classList.add('active-click', 'active-white');
+      } else {
+        nav.style.height = '0';
+        nav.style.display = 'none';
+        icon.classList.remove('bi-x');
+        icon.classList.add('bi-list');
+        header.classList.remove('active-click', 'active-white');
+      }
+      setNavVisible(!navVisible);
+    };
+
+    const handleScroll = () => {
+      let scrollTop = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (scrollTop > 150) {
+        header.classList.add('active-scroll');
+      } else {
+        header.classList.remove('active-scroll');
+      }
+    };
+
+    navBtn.addEventListener('click', toggleNav);
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      navBtn.removeEventListener('click', toggleNav);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [navVisible]);
+
   return (
     <div>
-      <header class="active-scroll active-click active-white">
-          <div class="logo">
-              <div class="inner">
-                  <p><a href="#">EDEN<br/>C&S</a></p>
-
-                  <div class="nav_btn"><i class="bi bi-list"></i></div>
-              </div>
+      <header className="active-scroll active-click active-white">
+        <div className="logo">
+          <div className="inner">
+            <p><a href="#">EDEN<br/>C&S</a></p>
+            <div className="nav_btn"><i className="bi bi-list"></i></div>
           </div>
+        </div>
 
-          <nav>
-              <div class="menu">
-                  <ul>
-                      <li><a href="#section02" class="inner">참여품목</a></li>
-                      <li><a href="#section04" class="inner">입주민혜택</a></li>
-                      <li><a href="#section06" class="inner">전시회안내</a></li>
-                      <li><a href="#section08" class="inner">결제방법</a></li>
-                      <li><a href="#section09" class="inner">사전예약</a></li>
-                      <li><a href="#section" class="inner">고객센터</a></li>
-                  </ul>
-              </div>
-          </nav>
+        <nav>
+          <div className="menu">
+            <ul>
+              <li><a href="#section02" className="inner">참여품목</a></li>
+              <li><a href="#section04" className="inner">입주민혜택</a></li>
+              <li><a href="#section06" className="inner">전시회안내</a></li>
+              <li><a href="#section08" className="inner">결제방법</a></li>
+              <li><a href="#section09" className="inner">사전예약</a></li>
+              <li><a href="#section" className="inner">고객센터</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
     </div>
   );
