dfabc0a145180ceda951f1548b0f61733cbfc77f
hyj5076 <hyj5076@hanmail.net>
2023-09-19 08:23:55 +0900

    New: Add the new function
    shopList.js : infinite rolling effects

--- landing3/src/components/ShopList.js
+++ landing3/src/components/ShopList.js
@@ -1,6 +1,27 @@
 import './App.css';
+import { useEffect } from 'react';
 
 function ShopList() {  
+
+    useEffect(() => {
+        // 롤링 배너 복제본 생성
+        const roller = document.querySelector('.roller');
+        if (!roller) return;
+
+        roller.id = 'roller1';
+        
+        const clone = roller.cloneNode(true);
+        clone.id = 'roller2';
+        document.querySelector('.roller-wrap').appendChild(clone); //부착
+        
+        //원본, 복제본 배너 위치 지정
+        document.querySelector('#roller1').style.left = '0px';
+        document.querySelector('#roller2').style.left = `${document.querySelector('.roller > div').offsetWidth}px`;
+        
+        //클래스 할당
+        roller.classList.add('original');
+        clone.classList.add('clone');
+    }, []);
 
     const shopDataList = [
         {
