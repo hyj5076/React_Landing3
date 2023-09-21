3a69ad7b380e63262e56ee0cf2bdee84c338752e
hyj5076 <hyj5076@hanmail.net>
2023-09-22 05:57:55 +0900

    New function
    Submit.js input 유효성 검사

--- landing3/src/components/Submit.js
+++ landing3/src/components/Submit.js
@@ -5,15 +5,52 @@ import './Submit.css';
 
 function Submit() {
     const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+    const [isPopupActive, setIsPopupActive] = useState(false);
+    const [agreement, setAgreement] = useState(false);
     
     const [tel, setTel] = useState("");
     const [dong, setDong] = useState("");
     const [ho, setHo] = useState("");
+    const [name, setName] = useState("");
 
     const dongInputRef = useRef(null);
     const hoInputRef = useRef(null);
     const nameInputRef = useRef(null);
     const telInputRef = useRef(null);
+    const agreeIconRef = useRef(null);
+
+    // 팝업
+    const togglePopup = () => {
+        setIsPopupActive(!isPopupActive);
+    }
+
+    const validateForm = () => {
+        const fields = [
+            { value: dong, ref: dongInputRef, message: "입주예정 동을 입력해주세요." },
+            { value: ho, ref: hoInputRef, message: "입주예정 호를 입력해주세요." },
+            { value: name, ref: nameInputRef, message: "성명을 입력해주세요." },
+            { value: tel, ref: telInputRef, message: "연락처를 입력해주세요." },
+        ];
+    
+        for (let field of fields) {
+            if (!field.value) {
+                alert(field.message);
+                field.ref.current.focus();
+                return false;
+            }
+        }
+    
+        if (!agreement) {
+            alert("개인정보 활용 수집 이용에 동의하여야 사전예약이 가능합니다.");
+    
+            // 팝업 펼치기
+            setIsPopupActive(true);
+            return false;
+        }
+    
+        return true;
+    };
+    
 
     // 높이 변경
     useEffect(() => {
@@ -27,6 +64,7 @@ function Submit() {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
+
 
     // 숫자만 반환하는 함수
     const onlyNumbers = (value) => value.replace(/[^0-9]/g, "");
@@ -72,7 +110,14 @@ function Submit() {
             setTel(formatTel(newVal));
         }
     };
-    
+
+    // 초기화 버튼
+    const resetForm = () => {
+        setDong("");
+        setHo("");
+        setName("");
+        setTel("");
+    };
     
   return (
   <div id="main">
@@ -85,6 +130,7 @@ function Submit() {
 
                 <div className="input_text">
                     <input 
+                        id="dongInput"
                         ref={dongInputRef} 
                         type="text" 
                         value={dong}
@@ -93,6 +139,7 @@ function Submit() {
                         onChange={handleInputChange(setDong, dongInputRef, hoInputRef)} 
                     />
                     <input 
+                        id="hoInput"
                         ref={hoInputRef} 
                         type="text" 
                         value={ho}
@@ -101,6 +148,9 @@ function Submit() {
                         onChange={handleInputChange(setHo, hoInputRef, nameInputRef)} 
                     />
                     <input 
+                        id="nameInput"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                         ref={nameInputRef} 
                         type="text" 
                         placeholder="성명" 
@@ -119,18 +169,44 @@ function Submit() {
                 </div>
 
                 <div className="agree active-rotate">
-                    <h3>개인정보 활용 수집 이용 동의<i className="bi bi-caret-down-fill"></i></h3>
-                    <div className="active-popup">
+                    <h3 onClick={togglePopup}>
+                        개인정보 활용 수집 이용 동의
+                        <i 
+                            ref={agreeIconRef}
+                            className={`bi ${isPopupActive ? 'bi-caret-up-fill' : 'bi-caret-down-fill'}`}
+                        />
+                    </h3>
+                    <div 
+                        className={`active-popup ${isPopupActive ? 'open' : ''}`} 
+                    >
                         <p>1. 개인정보 활용, 이용의 목적 : 소유주 명부 등재, 채팅방 및 네이버 카페, 밴드 등 초대, 홍보, 안내, 설문. 경품추첨 당첨 시 입주민 여부 확인, 비상 시 연락 등 입주 박람회 관련 업무 일체</p>
                         <p>2. 개인정보 목록 : 이름, 연락처(휴대전화번호), 주소, 소유 동 호수</p>
                         <p>3. 개인정보의 처리 및 보유기간 : 3년</p>
                         <p>4. 개인정보 제공을 거부할 수 있다는 사실 및 거부 시의 불이익 고지 : 상기 정보의 정보를 거부할 수 있으나, 입주박람회 경품 추첨에 참여할 수 없고, 이든씨엔에스의 홍보, 정보, 진행단계, 안내를 받을 수 없음.</p>
-                        <p><input id="agreementCheckbox" type="checkbox" /> 위 사항을 모두 읽었으며, 개인정보 활용에 동의함</p>
+                        <p>
+                        <input 
+                            id="agreementCheckbox" 
+                            type="checkbox"
+                            onChange={(e) => setAgreement(e.target.checked)}
+                         /> 
+                         위 사항을 모두 읽었으며, 개인정보 활용에 동의함</p>
                     </div>
                 </div>
                 <div className="btn">
-                    <Link className="submit_btn" to='/Success' onclick="return validateForm()">신청하기</Link>
-                    <Link to="/submit" onclick="resetForm()">초기화</Link>
+                    <Link 
+                        className="submit_btn" 
+                        to='/Success' 
+                        onClick={(e) => {
+                            if (!validateForm()) {
+                                e.preventDefault();
+                            }
+                        }}>
+                    신청하기
+                    </Link>
+                    <Link 
+                        onClick={resetForm}
+                    >초기화
+                    </Link>
                 </div>
             </div>
         </div>
