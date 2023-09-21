8ef96a2d23a502a3b9b11f1d8a39c8451f0e1400
hyj5076 <hyj5076@hanmail.net>
2023-09-22 05:12:06 +0900

    New function
    Submit.js Input 유효성 검사

--- landing3/src/components/Submit.js
+++ landing3/src/components/Submit.js
@@ -1,22 +1,121 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useRef, useState, useEffect } from 'react';
 import './Submit.css';
 
 function Submit() {
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+    
+    const [tel, setTel] = useState("");
+    const [dong, setDong] = useState("");
+    const [ho, setHo] = useState("");
+
+    const dongInputRef = useRef(null);
+    const hoInputRef = useRef(null);
+    const nameInputRef = useRef(null);
+    const telInputRef = useRef(null);
+
+    // 높이 변경
+    useEffect(() => {
+        function handleResize() {
+            setWindowHeight(window.innerHeight);
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // 숫자만 반환하는 함수
+    const onlyNumbers = (value) => value.replace(/[^0-9]/g, "");
+
+    const handleInputChange = (setter, inputRef, nextInputRef) => (e) => {
+        const value = onlyNumbers(e.target.value);
+        setter(value); // 상태 업데이트
+
+        // 4자리가 되면 다음 인풋으로 넘김
+        if (inputRef.current && nextInputRef.current && value.length >= 4) {
+            nextInputRef.current.focus();
+        }
+    };
+
+    // 하이픈 자동 추가
+    const formatTel = (value) => {
+        const val = value.replace(/[^0-9]/g, "");
+        
+        if (val.length <= 3) {
+            return val;
+        } else if (val.length <= 7) {
+            return val.replace(/(\d{3})(\d{1,4})/, "$1-$2");
+        } else {
+            return val.replace(/(\d{3})(\d{4})(\d{1,4})/, "$1-$2-$3");
+        }
+    };
+
+    const handleTelInputChange = (e) => {
+        setTel(formatTel(e.target.value));
+    };
+
+    // 숫자 한개씩 지우기
+    const handleTelKeyDown = (e) => {
+        if (e.key === "Backspace") {
+            let newVal = tel.replace(/[^0-9]/g, "");
+            
+            if (tel.endsWith("-")) { // 만약 입력값이 하이픈으로 끝난다면
+                newVal = newVal.substring(0, newVal.length - 1); // 숫자를 하나 더 삭제
+            } else {
+                newVal = newVal.substring(0, newVal.length);
+            }
+            
+            setTel(formatTel(newVal));
+        }
+    };
+    
+    
   return (
   <div id="main">
     <section id="section00">
-        <div className="style1 inner-sub">
+        <div className="style1 inner-sub" style={{ height: `${windowHeight}px` }}>
         <div className="title">
                 <p className="logo"><Link to="/" >EDEN C&S</Link></p>
                 <h1>힐스테이트 푸르지오 수원</h1>
                 <p className="exhibition">입주전시회 사전예약</p>
 
                 <div className="input_text">
-                    <input id="dongInput" type="text" placeholder="입주예정 동" maxlength="4" />
-                    <input id="hoInput" type="text" placeholder="입주예정 호" maxlength="4" />
-                    <input id="nameInput" type="text" placeholder="성명" maxlength="10" />
-                    <input id="telInput" type="text" placeholder="연락처" maxlength="13" />
+                    <input 
+                        ref={dongInputRef} 
+                        type="text" 
+                        value={dong}
+                        placeholder="입주예정 동" 
+                        maxlength="4" 
+                        onChange={handleInputChange(setDong, dongInputRef, hoInputRef)} 
+                    />
+                    <input 
+                        ref={hoInputRef} 
+                        type="text" 
+                        value={ho}
+                        placeholder="입주예정 호" 
+                        maxlength="4" 
+                        onChange={handleInputChange(setHo, hoInputRef, nameInputRef)} 
+                    />
+                    <input 
+                        ref={nameInputRef} 
+                        type="text" 
+                        placeholder="성명" 
+                        maxlength="10" 
+                    />
+                    <input
+                        ref={telInputRef}
+                        id="telInput"
+                        type="text"
+                        value={tel}
+                        placeholder="연락처"
+                        maxlength="13"
+                        onChange={handleTelInputChange}
+                        onKeyDown={handleTelKeyDown}
+                    />
                 </div>
 
                 <div className="agree active-rotate">
@@ -30,7 +129,7 @@ function Submit() {
                     </div>
                 </div>
                 <div className="btn">
-                    <Link to='/Success' onclick="return validateForm()">신청하기</Link>
+                    <Link className="submit_btn" to='/Success' onclick="return validateForm()">신청하기</Link>
                     <Link to="/submit" onclick="resetForm()">초기화</Link>
                 </div>
             </div>
