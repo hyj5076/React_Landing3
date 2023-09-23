a82e2341d73e7dc1f4569dc04eb4f2546ef67b83
hyj5076 <hyj5076@hanmail.net>
2023-09-24 06:12:00 +0900

    Create api.js
    api.js

---
+++ landing3/src/api.js
@@ -0,0 +1,6 @@
+/* export async function ItemList() {
+    const response = await fetch('https://mongodb.com/hyj5076/landingitems');
+    const body = await response.json();
+    return body;
+  } */
+  
